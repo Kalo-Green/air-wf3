c01f6043af3cfc41dd5ca52df3947cb3354ad7c6
Kalo-Green <kalogreenstyle@gmail.com>
2022-11-29 09:48:50 +0100

    HomeController.php

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,13 @@
+<?php 
+// clik droit -> generate namespace for this fil 
+namespace App\Controller;
+
+// mise ne place d'un controller
+class HomeController {
+
+    public function accueil() {
+        dd(__METHOD__);
+        // dd = dunm() + did()
+    }
+
+}
