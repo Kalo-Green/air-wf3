7cb47ebfc3a65af3c011f0933113a06a7acae118
Kalo-Green <kalogreenstyle@gmail.com>
2022-11-29 15:08:13 +0100

    make:controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,12 +2,19 @@
 // clik droit -> generate namespace for this fil 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 // mise ne place d'un controller
 class HomeController {
 
-    public function accueil() {
-        dd(__METHOD__);
-        // dd = dunm() + did()
+    // Response
+    #[Route('/', name: 'home')]
+    public function accueil() : Response { 
+        dump(__METHOD__);
+        // on retounre un objet de class Response
+        return new Response('OK');
+        // dd = dump() + did() ??
     }
 
 }
---
+++ src/Controller/TestController.php
@@ -0,0 +1,19 @@
+<?php
+// créer avec la commande : symfony console make:controller
+// puis on tape le nom du controller (ici test)
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends AbstractController
+{
+    #[Route('/test', name: 'app_test')]
+    public function index(): Response
+    {
+        return $this->render('test/index.html.twig', [
+            'controller_name' => 'TestController',
+        ]);
+    }
+}
