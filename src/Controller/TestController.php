98458da52ea7a7ccfc3e655fd2eb349c810b09f6
Kalo-Green <kalogreenstyle@gmail.com>
2022-11-29 16:24:12 +0100

    boucle

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -12,8 +12,21 @@ class TestController extends AbstractController
     #[Route('/test', name: 'app_test')]
     public function index(): Response
     {
+
+        // logique 
+        // accès BDD
+        // calcul
+        $tableau = [
+            1 => ['nom' => 'bleu', 'code' => '#0000ff'],
+            2 => ['nom' => 'blanc', 'code' => '#ffffff'],
+            3 => ['nom' => 'rouge', 'code' => '#ff0000'],
+        ];
+
+
         return $this->render('test/index.html.twig', [
-            'controller_name' => 'TestController',
+            'controller_name'   => 'TestController',
+            'test'              => 'Une valeur',
+            'liste'             => $tableau,
         ]);
     }
 }
