5b37a120e26f8fa0de7cc2dff73e640fdf97dbf3
Kalo-Green <kalogreenstyle@gmail.com>
2022-11-30 15:55:02 +0100

    éxexuter la migration

---
+++ src/Entity/Aeroport.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AeroportRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AeroportRepository::class)]
+class Aeroport
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 32)]
+    private ?string $nom = null;
+
+    #[ORM\Column]
+    private ?int $nb_pistes = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getNbPistes(): ?int
+    {
+        return $this->nb_pistes;
+    }
+
+    public function setNbPistes(int $nb_pistes): self
+    {
+        $this->nb_pistes = $nb_pistes;
+
+        return $this;
+    }
+}
